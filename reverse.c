c55bf2e6b597ec728e8ed97eabd78bd1e4e3d9d0
SSubah28 <140300837+SSubah28@users.noreply.github.com>
2023-07-23 12:52:32 +0600

    Add files via upload

--- reverse.c
+++ reverse.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
+int uwu(int arr[]){
+    int a,i=0,j=8;
+
+ while(i<j){
+ a = arr[i];
+ arr[i] = arr[j];
+ arr[j]= a;
+ i++;
+ j--;}
+
+ for(i=0;i<9;i++)
+ printf("%d\t",arr[i]);
+
+
+
+}
+
+int main(){
+ int arr [100];
+ int a,b,c,d,e,i,j,k;
+
+ for(i=0;i<9;i++)
+ scanf("%d",&arr[i]);
+
+
+uwu( arr);
+
+
+
+
+
+
+return 0;}
+
 
