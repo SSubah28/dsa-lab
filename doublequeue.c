c52cf5fd49c19f2b9ed20ef9c8cb84bf7633f430
SSubah28 <140300837+SSubah28@users.noreply.github.com>
2023-09-17 13:08:54 +0600

    Update doublequeue.c

--- doublequeue.c
+++ doublequeue.c
@@ -2,27 +2,22 @@
 #define size 5
 int queue[size];
 int front=-1, rear = -1;
-
 int isfull()
 {
     if(front ==rear+1 ||(front == 0 &&rear == size-1))
-        return 1;
+    return 1;
     return 0;
-    }
-
-
+}
 int isempty()
 {
     if(front==-1)
-        return 1;
+    return 1;
     else return 0;
 }
-
-
 void enqf(int a)
 {
     if(isfull())
-        printf("Queue is full!!\n");
+    printf("Queue is full!!\n");
     else if(front==-1&&rear ==-1)
     {
         front = rear = 0;
@@ -31,22 +26,18 @@ void enqf(int a)
     else if(front==0)
     {
         front = size -1;
-
         queue[front]= a;
     }
-
-else
+    else
     {
         front--;
         queue[front]= a;
     }
 }
-
 void enqr(int a)
 {
     if(isfull())
-        printf("Queue is full!!\n");
-
+    printf("Queue is full!!\n");
     else if(front==-1&&rear ==-1)
     {
         front = rear = 0;
@@ -56,13 +47,8 @@ void enqr(int a)
     {
         rear++;
         queue[rear]= a;
-
     }
-
-
 }
-
-
 void deqf()
 {
     if( isempty())
@@ -70,22 +56,16 @@ void deqf()
         printf("Queue is empty!!\n");
     }
     else if(front==rear)
-
     {
         printf("Deleted value is %d\n",queue[front]);
         front=rear=-1;
-
     }
-
-
     else
     {
         printf("Deleted value is %d\n",queue[front]);
         front = (front+1)%size;
-
     }
 }
-
 void deqr()
 {
     if( isempty())
@@ -97,36 +77,28 @@ void deqr()
         printf("Deleted value is %d\n",queue[rear]);
         front=rear=-1;
     }
-
     else
     {
         printf("Deleted value is %d\n",queue[rear]);
         rear = (rear-1)%size;
-
     }
 }
-
-
-
 void disp()
 {
     int i;
-
     for(i=front; i!=rear; i=(i+1)%size)
     {
         printf("%d \t",queue[i]);
     }
     printf("%d",queue[rear]);
-
     printf("\n");
 }
-
-getf()
+void getf()
 {
     printf("Front is %d\n",queue[front]);
 }
 
-getr()
+void getr()
 {
     printf("Rear is %d\n",queue[rear]);
 }
@@ -134,12 +106,11 @@ getr()
 int main()
 {
     deqr();
-        deqf();
+    deqf();
     enqr(5);
     enqr(12);
     enqr(15);
     disp();
-
     enqf(23);
     enqf(34);
     enqf(16);
